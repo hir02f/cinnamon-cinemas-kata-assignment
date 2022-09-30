af6441687c173c24a10a9219ffcc0bd5ced0d9b4
Jas <doggrell@gmail.com>
2022-09-30 12:39:39 +0100

    Test two subsequent bookings

--- CinnamonCinemas.Tests/BookingTests.cs
+++ CinnamonCinemas.Tests/BookingTests.cs
@@ -10,7 +10,7 @@ public class BookingTests
     [Test]
     public void Make_A_Booking_Where_Not_Enough_Seats_Left()
     {
-        BookingManager bk = new BookingManager();
+        BookingManager bk = new ();
         var ex = Assert.Throws<ApplicationException>(() => bk.MakeBooking(16, "Customer1"));
         Assert.That(ex.Message, Is.EqualTo("Not enough seats left!"));
     }
@@ -18,7 +18,7 @@ public class BookingTests
     [Test]
     public void Make_A_Valid_Booking_Of_One_Seat()
     {
-        BookingManager bk = new BookingManager();
+        BookingManager bk = new ();
         bk.MakeBooking(1, "Customer1");
         bk.SeatingInfo.ElementAt(0).SeatNumber.Should().Be("A1");
         bk.SeatingInfo.ElementAt(0).CustomerName.Should().Be("Customer1");
@@ -28,7 +28,7 @@ public class BookingTests
     [Test]
     public void Make_A_Valid_Booking_Of_Two_Seats()
     {
-        BookingManager bk = new BookingManager();
+        BookingManager bk = new ();
         bk.MakeBooking(2, "Customer1");
         bk.SeatingInfo.ElementAt(0).SeatNumber.Should().Be("A1");
         bk.SeatingInfo.ElementAt(0).CustomerName.Should().Be("Customer1");
@@ -39,7 +39,7 @@ public class BookingTests
     [Test]
     public void Make_A_Valid_Booking_Of_Three_Seats()
     {
-        BookingManager bk = new BookingManager();
+        BookingManager bk = new ();
         bk.MakeBooking(3, "Customer1");
         bk.SeatingInfo.ElementAt(0).SeatNumber.Should().Be("A1");
         bk.SeatingInfo.ElementAt(0).CustomerName.Should().Be("Customer1");
@@ -47,5 +47,21 @@ public class BookingTests
         bk.SeatingInfo.ElementAt(1).CustomerName.Should().Be("Customer1");
         bk.SeatingInfo.ElementAt(2).SeatNumber.Should().Be("A3");
         bk.SeatingInfo.ElementAt(2).CustomerName.Should().Be("Customer1");
+    }
+
+    [Test]
+    public void Make_A_Valid_Booking_Of_One_Seat_Then_Three_Seats()
+    {
+        BookingManager bk = new();
+        bk.MakeBooking(1, "Customer1");
+        bk.SeatingInfo.ElementAt(0).SeatNumber.Should().Be("A1");
+        bk.SeatingInfo.ElementAt(0).CustomerName.Should().Be("Customer1");
+        bk.MakeBooking(3, "Customer2");
+        bk.SeatingInfo.ElementAt(1).SeatNumber.Should().Be("A2");
+        bk.SeatingInfo.ElementAt(1).CustomerName.Should().Be("Customer2");
+        bk.SeatingInfo.ElementAt(2).SeatNumber.Should().Be("A3");
+        bk.SeatingInfo.ElementAt(2).CustomerName.Should().Be("Customer2");
+        bk.SeatingInfo.ElementAt(3).SeatNumber.Should().Be("A4");
+        bk.SeatingInfo.ElementAt(3).CustomerName.Should().Be("Customer2");
     }
 }
